d96d730a50a6b7d45726b9b15ddf56a7a2664729
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:32:15 -0600

    add tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,3 +5,8 @@ it("Sets the office number using the constructor", () => {
     const employee = new Manager("David", "001", "david@company.com", "101");
     expect(employee.officeNumber).toBe("101");
 })
+
+it("Gets the office number from the getOfficeNumber method", () => {
+    const employee = new Manager("David", "001", "david@company.com", "101");
+    expect(employee.getOfficeNumber()).toBe("101");
+})
