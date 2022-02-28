3afae77dd9ec0cc0649e2ca6c325e643db828a9b
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:37:22 -0600

    Tests for Intern

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,6 @@
+const Intern = require("../lib/Intern");
+
+it("fetches the employees school from the constructor", () => {
+    const employee = new Intern("David", "001", "david@company.com", "Vanderbilt University");
+    expect(employee.school).toBe("Vanderbilt University");
+})
