05342f1432e7c5de4b1c475f8593cb0d68d355ee
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:22:14 -0600

    more tests for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,5 +2,16 @@ const Engineer = require("../lib/Engineer");
 
 it("fetches the employees GitHub username from the constructor", () => {
     const employee = new Engineer("David", "001", "david@company.com", "gitUsername");
-    expect(employee.githubUsername).toBe("gitUsername");
+    expect(employee.github).toBe("gitUsername");
 })
+
+it("fetches the employees GitHub username from the getGithub() method", () => {
+    const employee = new Engineer("David", "001", "david@company.com", "gitUsername");
+    expect(employee.getGithub()).toBe("gitUsername");
+}) 
+
+it("fetches the employees role from the getRole() method", () => {
+    const role = "Engineer";
+    const employee = new Engineer("David", "001", "david@company.com", "gitUsername");
+    expect(employee.getRole()).toBe(role);
+}) 
