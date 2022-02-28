a9f77b4037292cf5fef1e52aaeb2233a7df865af
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:38:50 -0600

    Intern test for getSchool() method

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,3 +4,8 @@ it("fetches the employees school from the constructor", () => {
     const employee = new Intern("David", "001", "david@company.com", "Vanderbilt University");
     expect(employee.school).toBe("Vanderbilt University");
 })
+
+it("fetches the employees school from the getSchool() method", () => {
+    const employee = new Intern("David", "001", "david@company.com", "Vanderbilt University");
+    expect(employee.getSchool()).toBe("Vanderbilt University");
+})
