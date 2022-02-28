9a03427948f1ad914a56de7647c083b530ea1cbc
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:06:11 -0600

    Add tests for methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,6 @@
 const Employee = require('../lib/Employee')
 
+// Test that constructor works
 it("returns employee's name as a string", () => {
     const employee = new Employee("David");
     expect (employee.name).toBe("David");
@@ -19,3 +20,24 @@ it("returns employee's role as a string", () => {
     const employee = new Employee("David", "001", "david@company.com", "Employee");
     expect (employee.role).toBe("Employee");
 });
+
+// Check that methods work
+it("fetches the employee's name via the getName() method", () => {
+    const employee = new Employee("David");
+    expect(employee.getName()).toBe("David");
+})
+
+it("fetches the employee's ID via the getId() method", () => {
+    const employee = new Employee("David", "001");
+    expect(employee.getId()).toBe("001");
+})
+
+it("fetches the employee's email via the getEmail() method", () => {
+    const employee = new Employee("David", "001", "david@company.com");
+    expect(employee.getEmail()).toBe("david@company.com");
+})
+
+it("fetches the employee's role via the getRole() method", () => {
+    const employee = new Employee("David", "001", "david@company.com", "Employee");
+    expect(employee.getRole()).toBe("Employee");
+})
