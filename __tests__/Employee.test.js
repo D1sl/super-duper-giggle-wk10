9a9ee8298145a29b7c9ec5e26b88ec87cac32ffe
D1sl <94911312+D1sl@users.noreply.github.com>
2022-02-27 20:00:28 -0600

    More tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,3 +4,18 @@ it("returns employee's name as a string", () => {
     const employee = new Employee("David");
     expect (employee.name).toBe("David");
 });
+
+it("returns employee's ID as a string", () => {
+    const employee = new Employee("David", "001");
+    expect (employee.id).toBe("001");
+});
+
+it("returns employee's email as a string", () => {
+    const employee = new Employee("David", "001", "david@company.com");
+    expect (employee.email).toBe("david@company.com");
+});
+
+it("returns employee's role as a string", () => {
+    const employee = new Employee("David", "001", "david@company.com", "Employee");
+    expect (employee.role).toBe("Employee");
+});
